d97c9e6cbb2b7844f6dc95ac16a61fd1b3255cec
copych <70332557+copych@users.noreply.github.com>
2022-11-14 10:52:26 +0300

    var renaming

--- ESP32-dualcore-i2s.ino
+++ ESP32-dualcore-i2s.ino
@@ -15,19 +15,21 @@
 
 const i2s_port_t i2s_num = I2S_NUM_0; // i2s port number
 
-TaskHandle_t Task0;
 TaskHandle_t Task1;
+TaskHandle_t Task2;
+size_t bytes_written;
 
 // Accumulated phase
 static float p1 = 0.0f;
 static float p2 = 0.0f;
 
 // Output buffers (2ch interleaved)
-static uint16_t out_buf0[DMA_BUF_LEN * 2];
-static uint16_t out_buf1[DMA_BUF_LEN * 2];
+static uint16_t synth_buf1[DMA_BUF_LEN * 2];
+static uint16_t synth_buf2[DMA_BUF_LEN * 2];
+static uint16_t out_buf[DMA_BUF_LEN * 2];
 
 
-static void synth0() {
+static void synth1() {
     float samp = 0.0f;
 
     for (int i=0; i < DMA_BUF_LEN; i++) {
@@ -38,12 +40,12 @@ static void synth0() {
       if (p1 >= TWOPI) {
           p1 -= TWOPI ;
       }      
-      out_buf0[i*2] = (uint16_t)(samp * 0.8f);
-      out_buf0[i*2+1] = (uint16_t)(samp * 0.2f);
+      synth_buf1[i*2] = (uint16_t)(samp * 0.8f);
+      synth_buf1[i*2+1] = (uint16_t)(samp * 0.2f);
     }
 }
 
-static void synth1() {
+static void synth2() {
     float samp = 0.0f;
 
     for (int i=0; i < DMA_BUF_LEN; i++) {
@@ -54,43 +56,42 @@ static void synth1() {
         if (p2 >= TWOPI) {
             p2 -= TWOPI ;
         }
-        out_buf1[i*2] = (uint16_t)(samp * 0.2f);
-        out_buf1[i*2+1] = (uint16_t)(samp * 0.8f);
+        synth_buf2[i*2] = (uint16_t)(samp * 0.2f);
+        synth_buf2[i*2+1] = (uint16_t)(samp * 0.8f);
     }
 }
 
-static void mixer() { // sum buffers and output the result
-    size_t bytes_written;
+static void mixer() { // sum buffers 
     for (int i=0; i < DMA_BUF_LEN; i++) { 
-        out_buf0[i*2] += out_buf1[i*2];
-        out_buf0[i*2+1] += out_buf1[i*2+1] ;
+        out_buf[i*2] = synth_buf1[i*2] + synth_buf2[i*2];
+        out_buf[i*2+1] = synth_buf1[i*2+1] + synth_buf2[i*2+1] ;
     }
-    
-    // now out_buf0 is ready, output
-    i2s_write(i2s_num, out_buf0, sizeof(out_buf0), &bytes_written, portMAX_DELAY);
 }
 
 // Core0 task
-static void audio_task0(void *userData) {
+static void audio_task1(void *userData) {
     while(1) {
         // this part of the code never intersects with mixer()
-        synth0();
+        synth1();
         
         // this part of the code is operating with shared resources, so we should make it safe
         if (ulTaskNotifyTake(pdTRUE, portMAX_DELAY)){
             mixer();
-            xTaskNotifyGive(Task1);
+            xTaskNotifyGive(Task2); // if you have glitches, you may place this string in the end of audio_task1
         }
+        
+        // now out_buf is ready, output
+        i2s_write(i2s_num, out_buf, sizeof(out_buf), &bytes_written, portMAX_DELAY);
     }
 }
 
 // task for Core1, which tipically runs user's code on ESP32
-static void audio_task1(void *userData) {
+static void audio_task2(void *userData) {
     while(1) {
-        // we can run it together with synth0(), but not with mixer()
+        // we can run it together with synth1(), but not with mixer()
         if (ulTaskNotifyTake(pdTRUE, portMAX_DELAY)) {
-            synth1();
-            xTaskNotifyGive(Task0);
+            synth2();
+            xTaskNotifyGive(Task1);
         }
     }
 }
@@ -123,26 +124,26 @@ void setup(void)
   i2s_set_pin(i2s_num, &i2s_pin_config); 
 
   xTaskCreatePinnedToCore(
-                  audio_task0,  // function 
-                  "Task0",      // name 
-                  1024,         // stack size 
-                  NULL,         // params 
-                  1,            // priority 
-                  &Task0,       // handle 
-                  0);           // Core ID 
-                      
-  xTaskCreatePinnedToCore(
                   audio_task1,  // function 
                   "Task1",      // name 
                   1024,         // stack size 
                   NULL,         // params 
                   1,            // priority 
                   &Task1,       // handle 
+                  0);           // Core ID 
+                      
+  xTaskCreatePinnedToCore(
+                  audio_task2,  // function 
+                  "Task2",      // name 
+                  1024,         // stack size 
+                  NULL,         // params 
+                  1,            // priority 
+                  &Task2,       // handle 
                   1);           // Core ID 
 
   // somehow we should allow tasks to run
-  xTaskNotifyGive(Task0);
   xTaskNotifyGive(Task1);
+  xTaskNotifyGive(Task2);
      
 }
 
